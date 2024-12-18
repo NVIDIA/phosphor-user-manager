831e180178d094ef92d7b79b61decc0c2fa87f34
Patrick Williams <patrick@stwcx.xyz>
2024-12-18 11:21:29 -0500

    clang-format: re-format for clang-19
    clang-format-19 isn't compatible with the clang-format-18 output, so we
    need to reformat the code with the latest version.  A few parameters
    in clang-tidy have been deprecated, so adjust the style file
    accordingly.
    
    See Ie2f6eb3b043f2d655c9df806815afd7971fd0947 for updated style.
    See I88192b41ab7a95599a90915013579608af7bc56f for clang-19 enablement.
    
    Change-Id: I73ef7ee6dc13498d3fcbee4e5971028e61031ad0
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- user_mgr.cpp
+++ user_mgr.cpp
@@ -299,8 +299,8 @@ void UserMgr::throwForUserNameConstraints(
     }
 }
 
-void UserMgr::throwForMaxGrpUserCount(
-    const std::vector<std::string>& groupNames)
+void
+    UserMgr::throwForMaxGrpUserCount(const std::vector<std::string>& groupNames)
 {
     if (std::find(groupNames.begin(), groupNames.end(), "ipmi") !=
         groupNames.end())
@@ -1032,8 +1032,7 @@ bool UserMgr::userPasswordExpired(const std::string& userName)
     // All user management lock has to be based on /etc/shadow
     // TODO  phosphor-user-manager#10 phosphor::user::shadow::Lock lock{};
 
-    struct spwd spwd
-    {};
+    struct spwd spwd{};
     struct spwd* spwdPtr = nullptr;
     auto buflen = sysconf(_SC_GETPW_R_SIZE_MAX);
     if (buflen < 0)
