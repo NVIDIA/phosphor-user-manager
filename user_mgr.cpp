473a34d07e0e833f1e6edd2eb38462a46fd38296
Patrick Williams <patrick@stwcx.xyz>
2025-02-01 08:22:37 -0500

    clang-format: update latest spec and reformat
    Copy the latest format file from the docs repository and apply.
    
    Change-Id: I295ded87d74037b2f4a6c4b85d82af20d49ae4a7
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- user_mgr.cpp
+++ user_mgr.cpp
@@ -299,8 +299,8 @@ void UserMgr::throwForUserNameConstraints(
     }
 }
 
-void
-    UserMgr::throwForMaxGrpUserCount(const std::vector<std::string>& groupNames)
+void UserMgr::throwForMaxGrpUserCount(
+    const std::vector<std::string>& groupNames)
 {
     if (std::find(groupNames.begin(), groupNames.end(), "ipmi") !=
         groupNames.end())
