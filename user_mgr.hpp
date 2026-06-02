c5420e019ea6ce49ccba9102f529d2b9c8874e76
Chandramohan Harkude <charkude@nvidia.com>
2026-06-02 08:07:55 -0700

    user-mgr: prevent deletion of user with uid 0
    Deleting the user with uid 0 (often known as "root") would render the
    system unmanageable, regardless of their username.
    
    Add throwForUidZero() which looks up the user's passwd entry and
    throws NotAllowed if the UID is 0. The UID is checked rather than the
    username because UID 0 is not required to be named "root". Downstream
    forks may name the user "sysadmin" instead.
    
    Tested: Deleting the root user via busctl call fails with "The operation
            is not allowed" successfully.
    
    Change-Id: I62d61c05cfad1b703ef7b4b56badc80f8d396da4
    Signed-off-by: Tan Siewert <tan.siewert@9elements.com>
    
    
    (cherry picked from commit 88c45a93b0d74522e6657870e6a56e6735d8f9b7)
    
    Co-authored-by: Tan Siewert <tan.siewert@9elements.com>

--- user_mgr.hpp
+++ user_mgr.hpp
@@ -519,6 +519,14 @@ class UserMgr : public Ifaces
     virtual bool isUserExistSystem(const std::string& userName);
     size_t getNonIpmiUsersCount();
 
+    /** @brief check user has UID 0
+     *  method to check whether user is not UID 0 (i.e. root),
+     *  and throw if it is.
+     *
+     *  @param[in] userName - name of the user
+     */
+    void throwForUidZero(const std::string& userName);
+
     /** @brief check user exists
      *  method to check whether user exist, and throw if not.
      *
